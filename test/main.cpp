5763c4d9d72b0b7577a402ec6c075278c1e5d90f
WilliamLindskog <williamlindskog@protonmail.com>
2024-03-15 09:44:10 +0100

    Basic menu for actions

---
+++ test/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    // 1. Print help
+    cout << "1. Print Help" << endl;
+
+    // 2. Print Exchange Statistics
+    cout << "2. Print Exchange Statistics" << endl;
+
+    // 3. Make an offer
+    cout << "3. Make an offer" << endl;
+
+    // 4. Make a bid
+    cout << "4. Make a bid" << endl;
+
+    // 5. Print Wallet 
+    cout << "5. Print Wallet" << endl;
+
+    // 6. Continue 
+    cout << "6. Continue" << endl;
+    cout << "Type in 1 - 6" << endl;
+
+    int UserInput;
+    cin >> UserInput;
+    cout << "You entered: " << UserInput << endl;
+
+    return 0;
+}
